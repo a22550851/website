51995e93bb3d84b6160f904b0b717a2057f699e5
a22550851 <81280769+a22550851@users.noreply.github.com>
2026-01-02 11:55:02 +0800

    Update events.js

--- prediction oled/events.js
+++ prediction oled/events.js
@@ -594,7 +594,7 @@ async function fetchPropertiesPredictionData(emitter_smiles = null) {
     const bodyData = {};
     if (emitter_smiles) bodyData.emitter_smiles = emitter_smiles;
 
-    const response = await fetch('https://polymer-ml-platform-server.site/OLED_properties_predict', {
+    const response = await fetch('http://polymeraiplatform.site/OLED_properties_predict', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(bodyData)
@@ -1439,3 +1439,4 @@ function changeCanvasLabelColor(canvasId, color) {
         canvasLabel.style.color = color;
     }
 }
+
